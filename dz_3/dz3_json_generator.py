279820eb177967909744c46ceae741eb3ed8402f
bloodmilk44 <59761531+bloodmilk44@users.noreply.github.com>
2021-11-02 22:31:41 +0400

    Исправления для правильной генерации JSON

--- dz_3/dz3_json_generator.py
+++ dz_3/dz3_json_generator.py
@@ -6,10 +6,10 @@ with open('../files/books.csv', newline='') as b:
     reader = DictReader(b)
 
     for row in reader:
-        title = row['Title']
-        author = row['Author']
-        height = row['Height']
-        # print(title, author, height)  #  С помощью 'print' получается составить последовательности из csv,
+        title = str(row['Title'])
+        author = str(row['Author'])
+        height = str(row['Height'])
+        print(json.dump(title, author, height))  #  С помощью 'print' получается составить последовательности из csv,
         # но при генерации JSON берется только одно значение, не разобрался как сделать.
 
 with open("../files/users.json", "r") as f:
@@ -19,11 +19,11 @@ for name in data_list:
     name_user = name['name']
     gender_user = name['gender']
     address_user = name['address']
-    # print(name_user, gender_user, address_user)
+    print(name_user, gender_user, address_user)
 
-    result = [{'name': name_user, 'gender': gender_user, 'address': address_user}]
+    result = [{'name': name_user, 'gender': gender_user, 'address': address_user, 'books': [{'title': title, 'author': author, 'height': height}]}]
     # result_2 = [{'title': title, 'author': author, 'height': height}]
-    # print(result)
+    print(result)
 
     with open("../files/example.json", "w") as g:
         json.dump(result, g, indent=4)  # JSON генерируется только по одной записи
