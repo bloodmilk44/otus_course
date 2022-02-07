c062557d1df2daee006c2f2bec55ab7195da94b7
bloodmilk44 <59761531+bloodmilk44@users.noreply.github.com>
2022-02-07 23:02:44 +0400

    Доработка скрипта
    Теперь работает функционал добавления книг к пользователям.
    Если количество книг меньше количества пользователей, то остальным добавляется пустой массив. Если книг больше чем пользователей, то раздача книг прекращается.

--- dz_3/dz3_json_generator.py
+++ dz_3/dz3_json_generator.py
@@ -5,12 +5,20 @@ from csv import DictReader
 with open('../files/books.csv', newline='') as b:
     reader = DictReader(b)
 
+    title_list = []
+    author_list = []
+    height_list = []
+
     for row in reader:
         title = str(row['Title'])
         author = str(row['Author'])
         height = str(row['Height'])
-        print(json.dump(title, author, height))  #  С помощью 'print' получается составить последовательности из csv,
-        # но при генерации JSON берется только одно значение, не разобрался как сделать.
+
+        #  Получаем списки всех книг, авторов и количества страниц
+        title_list.append(title)
+        author_list.append(author)
+        height_list.append(height)
+
 
 with open("../files/users.json", "r") as f:
     data_list = json.loads(f.read())
@@ -19,11 +27,16 @@ for name in data_list:
     name_user = name['name']
     gender_user = name['gender']
     address_user = name['address']
-    print(name_user, gender_user, address_user)
 
-    result = [{'name': name_user, 'gender': gender_user, 'address': address_user, 'books': [{'title': title, 'author': author, 'height': height}]}]
-    # result_2 = [{'title': title, 'author': author, 'height': height}]
+    #  Генерируем структуру для генерации JSON
+    result = json.dumps({'name': name_user, 'gender': gender_user, 'address': address_user,
+                         'books': [{'Title' : title_list.pop(),
+                                    'Author' : author_list.pop(),
+                                    'Height' : height_list.pop()}]}, indent=4)
+
+
     print(result)
+    #print(json.dumps(result))
+    #with open("../files/example.json", "w") as g:
+    #    json.dump(result, g, indent=4)
 
-    with open("../files/example.json", "w") as g:
-        json.dump(result, g, indent=4)  # JSON генерируется только по одной записи
