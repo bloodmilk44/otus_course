1b4f61c1e83ecfd1bdc057eb5939db212b965fee
bloodmilk44 <snow.tigraa@gmail.com>
2022-02-18 13:56:59 +0400

    Новая логика создания списков книг и пользователей

--- dz_3/dz3_json_generator.py
+++ dz_3/dz3_json_generator.py
@@ -2,42 +2,30 @@ import json
 from csv import DictReader
 
 
-with open('../files/books.csv', newline='') as b:
+with open('../files/books2.csv', newline='') as b:
     reader = DictReader(b)
 
     title_list = []
-    author_list = []
-    height_list = []
 
     for row in reader:
-        title = str(row['Title'])
-        author = str(row['Author'])
-        height = str(row['Height'])
-
-        #  Получаем списки всех книг, авторов и количества страниц
-        title_list.append(title)
-        author_list.append(author)
-        height_list.append(height)
-
+        test = {}
+        test['Title'] = str(row['Title'])
+        test['Author'] = str(row['Author'])
+        test['Height'] = str(row['Height'])
+        title_list.append(test)
 
 with open("../files/users.json", "r") as f:
     data_list = json.loads(f.read())
 
+
+result_2 = []
 for name in data_list:
-    name_user = name['name']
-    gender_user = name['gender']
-    address_user = name['address']
+    book = {}
+    book['name'] = name['name']
+    book['gender'] = name['gender']
+    book['address'] = name['address']
+    book['books'] = [title_list.pop(0)] if len(title_list) > 0 else []
+    result_2.append(book)
 
-    #  Генерируем структуру для генерации JSON
-    result = json.dumps({'name': name_user, 'gender': gender_user, 'address': address_user,
-                         'books': [{'Title' : title_list.pop(),
-                                    'Author' : author_list.pop(),
-                                    'Height' : height_list.pop()}]}, indent=4)
-
-    with open("../files/example.json", "w") as g:
-        for row in result:
-            print(result)
-            g.write(result)
-
-
-
+with open("../files/example.json", "w") as g:
+    g.write(json.dumps(result_2, indent=4))
